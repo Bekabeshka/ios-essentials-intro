c3bfda419ca77602183ef1da694b9c00765b7e0c
bekabeshka <4bekdaulet@gmail.com>
2023-02-12 16:11:07 +0600

    remove apiclient dependency

--- singleton.playground/Contents.swift
+++ singleton.playground/Contents.swift
@@ -1,6 +1,53 @@
+import UIKit
 import Foundation
 
-final class Api {
-    static let shared = Api()
+// Api Module
+final public class ApiClient {
+    static let instance = ApiClient()
     private init() { }
+    
+    public func execute(_ : URLRequest, completion: ((Data) -> Void)) { }
+}
+
+// Main Module
+extension ApiClient {
+    func login(completion: (LoggedInUser) -> Void) {
+        let request = URLRequest(url: .applicationDirectory)
+        self.execute(request, completion: { _ in })
+    }
+}
+
+extension ApiClient {
+    func loadFeed(completion: ([FeedItem]) -> Void) {
+        let request = URLRequest(url: .applicationDirectory)
+        self.execute(request, completion: { _ in })
+    }
+}
+
+// Login Module
+public struct LoggedInUser { }
+
+class LoginViewController: UIViewController {
+    var login: (((LoggedInUser) -> Void) -> Void)?
+    
+    func didTapLoginButton() {
+        login? { user in
+            // show feed screen
+        }
+    }
+}
+
+
+// Feed Module
+public struct FeedItem { }
+
+class FeedViewController: UIViewController {
+    var loadFeed: ((([FeedItem]) -> Void) -> Void)?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadFeed? { items in
+            // show items
+        }
+    }
 }
